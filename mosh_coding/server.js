f25296f72e66ee3cd3d3f6111f7ae311d4998a21
Frederic Rieg <fredericrieg@protonmail.com>
2023-07-25 10:49:49 +0300

    added post method & Joi validation

--- mosh_coding/server.js
+++ mosh_coding/server.js
@@ -1,10 +1,53 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
+app.use(express.json()); // Use that middleware in the request processing pipeline.
+
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+];
+
 app.get('/', (req, res) => {
     res.send('Hello World!!!');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send([courses]);
+});
+
+app.post('/api/courses', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.valid(req.body, schema);
+
+    if (result.error) { // 400 Bad Request
+        res.status(400).send(result.error.details[0].message);
+        return;
     }
-);
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course); // Add the new course to the courses array.
+    res.send(courses); // Return the new course to the client.
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) { res.status(404).send('Status 404 - The course with the given ID was not found.'); }
+    else { res.send(course); }
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.query);
+});
 
 // PORT
 const port = process.env.PORT || 3000
